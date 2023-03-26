3b3a8d11a860b1da8f2dbac4ad3cd2d8135f0591
Alexander <alexanderlysenko99@mail.ru>
2023-03-26 17:06:14 +0300

    createdPortal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from 'react';
+import styles from './app.module.css';
+import Modal from './Modal';
+export const App = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState('Aboba');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setOpen(true)}>Открыть/Закрыть</button>
+      <input value={text} onChange={(e) => setText(e.target.value)}></input>
+      <Modal open={open} onClose={() => setOpen(false)} text={text}></Modal>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,29 @@
+import { useEffect, useMemo } from 'react';
+import { createPortal } from 'react-dom';
+import style from './style.module.css';
+const modalRootElement = document.querySelector('#modal');
+
+const Modal = (props) => {
+  const { open, onClose, text } = props;
+  const element = useMemo(() => document.createElement('div'), []);
+  useEffect(() => {
+    if (open) {
+      modalRootElement.appendChild(element);
+
+      return () => {
+        modalRootElement.removeChild(element);
+      };
+    }
+  });
+  if (open) {
+    return createPortal(
+      <div className={style.modal_background} onClick={onClose}>
+        <div className={style.modal_card}>{text}</div>
+      </div>,
+      element
+    );
+  }
+  return null;
+};
+
+export default Modal;
